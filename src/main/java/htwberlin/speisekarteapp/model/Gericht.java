8bb70354b04b4d54b04db89c01310dd75d15e639
s0579111 <s0579111@htw-berlin.de>
2024-04-17 14:36:25 +0200

    Entity

--- src/main/java/htwberlin/speisekarteapp/model/Gericht.java
+++ src/main/java/htwberlin/speisekarteapp/model/Gericht.java
@@ -1,4 +1,15 @@
 package htwberlin.speisekarteapp.model;
 
 public class Gericht {
+    private int gerichtID;
+    private String name;
+    private String beschreibung;
+    private double preis;
+
+    public Gericht(String name, String beschreibung, double preis, boolean vegetarisch) {
+        this.gerichtID;
+        this.name = name;
+        this.preis = preis;
+      
+    }
 }
