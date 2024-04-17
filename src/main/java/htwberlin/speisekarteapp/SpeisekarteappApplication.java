23151ce9c6454096603fa277556bac3cec6c0f3e
Calvin Vi <Calvin.Vi@Student.HTW-Berlin.de>
2024-04-17 13:55:03 +0200

    first commit

---
+++ src/main/java/htwberlin/speisekarteapp/SpeisekarteappApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.speisekarteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeisekarteappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeisekarteappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/htwberlin/speisekarteapp/SpeisekarteappApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.speisekarteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeisekarteappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
