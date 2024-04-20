37d7a5a877f134463b67326fb58b5ff7cbf2b143
Calvin Vi <Calvin.Vi@Student.HTW-Berlin.de>
2024-04-20 12:55:28 +0200

    Calvin test commit

--- src/main/java/htwberlin/speisekarteapp/controller/UserController.java
+++ src/main/java/htwberlin/speisekarteapp/controller/UserController.java
@@ -1,5 +1,6 @@
 package htwberlin.speisekarteapp.controller;
 
+import htwberlin.speisekarteapp.model.Gericht;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 @RequestMapping
 public class UserController {
-    @GetMapping("Hello")
+    @GetMapping("/hello")
     public ResponseEntity<String> helloworld(@RequestParam("text") String text) {
-        return ResponseEntity.ok("Hello World" + text);
-    }}
+        return ResponseEntity.ok("Hello World"+ text );
+    }
+    @GetMapping("/gericht")
+    public ResponseEntity<Gericht> getGericht() {
+        // Annahme: Hier wird ein Gericht mit festen Werten erstellt
+        Gericht gericht = new Gericht("Pizza", "Pizza Margherita", 7,true);
+        return ResponseEntity.ok(gericht);
+    }
+}
--- src/main/java/htwberlin/speisekarteapp/model/Gericht.java
+++ src/main/java/htwberlin/speisekarteapp/model/Gericht.java
@@ -6,10 +6,42 @@ public class Gericht {
     private String beschreibung;
     private double preis;
 
-    public Gericht(String name, String beschreibung, double preis, boolean vegetarisch) {
+    public Gericht(String name, String beschreibung, double preis,boolean vegetarisch) {
         this.gerichtID = 0; // Assign a default value to gerichtID
         this.name = name;
         this.beschreibung = beschreibung;
         this.preis = preis;
     }
+    public int getGerichtID() {
+        return gerichtID;
+    }
+
+    public void setGerichtID(int gerichtID) {
+        this.gerichtID = gerichtID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public double getPreis() {
+        return preis;
+    }
+
+    public void setPreis(double preis) {
+        this.preis = preis;
+    }
 }
+
